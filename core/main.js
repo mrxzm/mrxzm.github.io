cb03722848df7cff35975f351e2cc310d6e09fdb
mrxzm <mrxzm@live.com>
2018-03-20 17:49:55 +0800

    消息反馈 删除掉原来的错误代码

--- core/main.js
+++ core/main.js
@@ -156,79 +156,31 @@ require(['core/config/extend_config.js'], function () {
         });
 
         /* ---------------------------------------------------------------------- */
-        /* -------------------------- Contact Form ------------------------------ */
+        /* -------------------------------- 留言 -------------------------------- */
         /* ---------------------------------------------------------------------- */
-
-        // Needed variables
         var $contactform = $('#contactform'),
-            $success = ' Your message has been sent. Thank you!';
+            $success = ' 谢谢你的反馈消息！';
 
-        $contactform.submit(function() {
-            $.ajax({
-                type: "POST",
-                url: "php/contact.php",
-                data: $(this).serialize(),
-                success: function(msg)
-                {
-                    var msg_error = msg.split(",");
-                    var output_error = '';
-
-                    if (msg_error.indexOf('error-message') != -1) {
-                        $("#contact-message").addClass("has-error");
-                        $("#contact-message").removeClass("has-success");
-                        output_error = 'Please enter your message.';
-                    } else {
-                        $("#contact-message").addClass("has-success");
-                        $("#contact-message").removeClass("has-error");
-                    }
-
-                    if (msg_error.indexOf('error-email') != -1) {
-
-                        $("#contact-email").addClass("has-error");
-                        $("#contact-email").removeClass("has-success");
-                        output_error = 'Please enter valid e-mail.';
-                    } else {
-                        $("#contact-email").addClass("has-success");
-                        $("#contact-email").removeClass("has-error");
-                    }
-
-                    if (msg_error.indexOf('error-name') != -1) {
-                        $("#contact-name").addClass("has-error");
-                        $("#contact-name").removeClass("has-success");
-                        output_error = 'Please enter your name.';
-                    } else {
-                        $("#contact-name").addClass("has-success");
-                        $("#contact-name").removeClass("has-error");
-                    }
-
-
-                    if (msg == 'success') {
-
-                        response = '<div class="alert alert-success success-send">' +
-                            '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
-                            '<i class="glyphicon glyphicon-ok" style="margin-right: 5px;"></i> ' + $success
-                            + '</div>';
-
-
-                        $(".reset").trigger('click');
-                        $("#contact-name").removeClass("has-success");
-                        $("#contact-email").removeClass("has-success");
-                        $("#contact-message").removeClass("has-success");
-
-                    } else {
-
-                        response = '<div class="alert alert-danger error-send">' +
-                            '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
-                            '<i class="glyphicon glyphicon-remove" style="margin-right: 5px;"></i> ' + output_error
-                            + '</div>';
-
-                    }
-                    // Hide any previous response text
-                    $(".error-send,.success-send").remove();
-                    // Show response message
-                    $contactform.prepend(response);
-                }
-            });
+        $contactform.submit(function () {
+            var name = $(this).find('input:text[name=name]').val();
+            var email = $(this).find('input:text[name=email]').val();
+            var content = $(this).find('textarea[name=message]').val();
+            var url = 'mailto:' + encodeURIComponent('mrxzm@live.com');
+            url += '?subject=' + encodeURIComponent('['+ name +'] mrxzm主页的反馈消息');
+            url += '&body=' + encodeURIComponent('姓名:' + name + '\n' +'联系方式:' + email + '\n\t' + content);
+            window.location = url;
+            // 成功消息
+            var responseSuccess = '<div class="alert alert-success success-send">' +
+                '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+                '<i class="glyphicon glyphicon-ok" style="margin-right: 5px;"></i> ' + $success
+                + '</div>';
+            // 错误消息
+            var responseError = '<div class="alert alert-danger error-send">' +
+                '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+                '<i class="glyphicon glyphicon-remove" style="margin-right: 5px;"></i> ' + output_error
+                + '</div>';
+            $(".error-send,.success-send").remove();
+            $contactform.prepend(responseSuccess);
             return false;
         });
 
@@ -444,20 +396,6 @@ require(['core/config/extend_config.js'], function () {
             var s = document.getElementsByTagName("script")[0];
             s.parentNode.insertBefore(hm, s);
         })();
-
-        /* ---------------------------------------------------------------------- */
-        /* -------------------------------- 留言 -------------------------------- */
-        /* ---------------------------------------------------------------------- */
-        $('#contactform').submit(function () {
-            var name = $(this).find('input:text[name=name]').val();
-            var email = $(this).find('input:text[name=email]').val();
-            var content = $(this).find('textarea[name=message]').val();
-            var url = 'mailto:' + encodeURIComponent('mrxzm@live.com');
-            url += '?subject=' + encodeURIComponent('['+ name +'] mrxzm主页的反馈消息');
-            url += '&body=' + encodeURIComponent('姓名:' + name + '\n' +'联系方式:' + email + '\n\t' + content);
-            window.location = url;
-            return true;
-        });
 
 
     });
